83da42ae589db94ff53aa12bee6015e2a5d018df
Weiyan Zhu <weiyanz@uci.edu>
2020-07-17 22:05:38 -0700

    init commit

---
+++ dovepotWeb/src/test/java/com/dovepot/dovepotWeb/DovepotWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dovepot.dovepotWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DovepotWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
