a12de1dc4b4131694d0fea314bf4327026eef8ea
Weiyan Zhu <weiyanz@uci.edu>
2020-07-18 12:25:24 -0700

    clean repo and create files

---
+++ dovepotWeb/src/main/resources/static/precache-manifest.2c108abf5dad984549ff78506007915b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669277e9a342bee5b67210e04a8b4034",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fe67a0300a472c4cd1a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc03aed87d75d818f89c",
+    "url": "/static/js/2.09f1b02a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.09f1b02a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fe67a0300a472c4cd1a",
+    "url": "/static/js/main.2326f788.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- dovepotWeb/src/main/resources/static/service-worker.js
+++ dovepotWeb/src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.68ccfaf0522b0adb06dd1d2ac9a3a6b8.js"
+  "/precache-manifest.2c108abf5dad984549ff78506007915b.js"
 );
 
 self.addEventListener('message', (event) => {
