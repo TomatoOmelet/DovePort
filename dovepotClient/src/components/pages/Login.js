c63c9fd238ed9f10c81ccd40542daa6bc8ce22ec
Weiyan Zhu <weiyanz@uci.edu>
2020-07-19 12:40:25 -0700

    import old context code

--- dovepotClient/src/components/pages/Login.js
+++ dovepotClient/src/components/pages/Login.js
@@ -1,23 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 import axios from "axios"
 
 const Login = () => {
+    const[state, setState] = useState({email: '',
+                                       password: '',
+                                       hasLoginFailed: false,
+                                       showSuccessMessage: false});
+
+    const onChange = (e) => {
+        setState(
+            {
+                ...state,
+                [e.target.name]: e.target.value
+            }
+        )
+    }
     //Login
     const login = async (formData) => {
         const config = {
             headers:{"ContentType":"application/json"}
         };
 
-        try {
-            const res = await axios.post("/perform_login", formData, config);
-        } catch (error) {
-            console.error(error.message)
+        // try {
+        //     const res = await axios.post("/perform_login", formData, config);
+        // } catch (error) {
+        //     console.error(error.message)
+        // }
+        if(state.email==='user' && state.password==='user'){
+            //AuthenticationService.registerSuccessfulLogin(state.email, state.password)
+            setState({...state, showSuccessMessage:true, hasLoginFailed:false})
+        }
+        else {
+             setState({...state, showSuccessMessage:false, hasLoginFailed:true})
         }
     }
 
     const onSubmit = (e)=>{
         e.preventDefault();
-        login();
+        login(state);
     }
 
     return (
@@ -27,11 +47,11 @@ const Login = () => {
             <form>
                 <div className="form-group">
                     <label htmlFor="email">Email:</label>
-                    <input type="email" className="form-control" name="email" required></input>
+                    <input type="email" className="form-control" name="email" onChange={onChange} required></input>
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password:</label>
-                    <input type="password" className="form-control" name="password" required></input>
+                    <input type="password" className="form-control" name="password" onChange={onChange} required></input>
                 </div>
                 <input type="submit" value="Login" className="btn btn-primary"/>
             </form>
