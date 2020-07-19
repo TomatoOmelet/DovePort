c399f4d15143693d7d08b15839d665eb8419b7cf
Weiyan Zhu <weiyanz@uci.edu>
2020-07-18 20:43:18 -0700

    Update Login.js

--- dovepotWeb/client/src/components/pages/Login.js
+++ dovepotWeb/client/src/components/pages/Login.js
@@ -1,8 +1,26 @@
 import React from 'react'
 
 const Login = () => {
+    //Login
+    const login = async (formData) => {
+        const config = {
+            headers:{"ContentType":"application/json"}
+        };
+
+        try {
+            const res = await axios.post("/perform_login", formData, config);
+        } catch (error) {
+            console.error(error.message)
+        }
+    }
+
+    const onSubmit = (e)=>{
+        e.preventDefault();
+        login();
+    }
+
     return (
-        <div className="form-container">
+        <div className="form-container" onSubmit={onSubmit}>
             <h1><span className="text-primary">Login</span></h1>
             
             <form>
