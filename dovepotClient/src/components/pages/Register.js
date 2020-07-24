3b64a829a797a3fb133ce933a2cc8c89af1b86df
Weiyan Zhu <weiyanz@berkeley.edu>
2020-07-23 21:32:34 -0700

    register feature

--- dovepotClient/src/components/pages/Register.js
+++ dovepotClient/src/components/pages/Register.js
@@ -1,26 +1,58 @@
-import React from 'react'
+import React, {useState, useContext, useEffect} from 'react'
+import AuthContext from "../../context/auth/authContext"
 
-const Register = () => {
+
+const Register = (props) => {
+    const[user, setUser] = useState({username: '', display_name:'', password: '', password2: ''});
+    const authContext = useContext(AuthContext);
+    const {register, isAuthenticated} = authContext;
+
+    useEffect(()=>{
+        if(isAuthenticated){
+            props.history.push("/");
+        }
+    }, [isAuthenticated])
+
+    const onSubmit = (e)=>{
+        e.preventDefault();
+        if(user.password === user.password2)
+        {
+            register({"username":user.username, "name":user.display_name, "password":user.password});
+        }else{
+            console.error("passwords does not match");
+        }
+        
+    }
+
+    const onChange = (e) => {
+        setUser(
+            {
+                ...user,
+                [e.target.name]: e.target.value
+            }
+        )
+    }
+
     return (
         <div className="form-container">
             <h1>Account <span className="text-primary">Register</span></h1>
         
-            <form onSubmit>
+            <form onSubmit={onSubmit}>
                 <div className="form-group">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" className="form-control" name="name" required></input>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" className="form-control" name="username" onChange={onChange} required minLength="6" maxLength="12"></input>
                 </div>
                 <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" className="form-control" name="email" required></input>
+                    <label htmlFor="display_name">Display Name</label>
+                    <input type="text" className="form-control" name="display_name" onChange={onChange} required></input>
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
-                    <input type="password" className="form-control" name="password" required minLength="6"></input>
+                    <input type="password" className="form-control" name="password" onChange={onChange} required minLength="6"></input>
                 </div>
                 <div className="form-group">
                     <label htmlFor="password2">Confirm Password</label>
-                    <input type="password" className="form-control" name="password2" required minLength="6"></input>
+                    <input type="password" className="form-control" name="password2" onChange={onChange} required minLength="6"></input>
                 </div>
                 <input type="submit" value="Register" className="btn btn-primary" />
             </form>
--- dovepotWeb/src/main/java/com/dovepot/dovepotWeb/controllers/UserController.java
+++ dovepotWeb/src/main/java/com/dovepot/dovepotWeb/controllers/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api")
+@CrossOrigin(origins={ "http://localhost:3000", "http://localhost:4200" })
 public class UserController {
     
     @Autowired
